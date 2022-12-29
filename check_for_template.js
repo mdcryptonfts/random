19670cc9c2ef307bc9e23d54b64d6c3bb56e23ab
mdcryptonfts <105838382+mdcryptonfts@users.noreply.github.com>
2022-12-29 09:03:08 -0500

    Update check_for_template.js

--- check_for_template.js
+++ check_for_template.js
@@ -12,6 +12,11 @@ const WAX_ACCOUNT_TO_CHECK = "mikedcrypto5"
 const API_PROVIDER = "https://wax.api.atomicassets.io"
 
 try{
+
+  if(COLLECTION_NAME.length == 0 || SCHEMA_NAME.length == 0 || WAX_ACCOUNT_TO_CHECK.length == 0 || TEMPLATE_ID.length == 0){
+    throw new Error('You left something blank');
+  }
+
   axios.get(API_PROVIDER + "/atomicassets/v1/assets?collection_name=" + COLLECTION_NAME + "&schema_name=" + SCHEMA_NAME + "&template_id=" + TEMPLATE_ID + "&owner=" + WAX_ACCOUNT_TO_CHECK + "&page=1&limit=100&order=desc&sort=asset_id")
   .then((res) => {
     console.log("User has " + res.data.data.length + " of this template")
